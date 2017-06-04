5332f9e353c1399552e5adef5e6aaf5e0397efdb
Steeve DELARDIERE <sdelardi@e1r4p18.42.fr>
2017-06-04 04:38:18 +0200

    Ended

---
+++ scripting/02.sh
@@ -0,0 +1,1 @@
+echo "0 4 * * 1 apt-get update && apt-get upgrade >> /var/log/update.script.log" >> /etc/crontab
---
+++ scripting/04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -e /etc/crontemp ]
+then
+	echo "0 0 * * * sh `pwd`/04.sh" >> /etc/crontab
+	cp /etc/crontab /etc/crontemp
+else
+	diff /etc/crontemp /etc/crontab > difftmp
+	if [ $? -ne 0 ]
+	then
+		mail -s "Crontab changed !" user < difftmp
+		rm difftmp
+		rm -f /etc/crontemp
+		cp /etc/crontab /etc/crontemp
+	fi
+fi
